70896c57b9954f5501faac209bc701f3bbe94e84
tkim-061203 <nntkim.work@gmail.com>
2024-09-04 16:20:21 +0700

    edit from rasp

--- src/read_csv.py
+++ src/read_csv.py
@@ -31,13 +31,25 @@ servo = motor_config()
 servo.relax_all_motors()
 servo.moveAbsAngle(servo.back_left_hip, 0)
 
-for i in range(len(BL_1_array)):
-    print(BL_1_array[i],BL_2_array[i],BL_3_array[i])
-    servo.moveAbsAngle(servo.back_left_hip, 180)
-    servo.moveAbsAngle(servo.back_left_upper, BL_2_array[i])
-    servo.moveAbsAngle(servo.back_left_lower, BL_3_array[i])
+# for i in range(len(BL_1_array)):
+#     print(BL_1_array[i],BL_2_array[i],BL_3_array[i])
+#     servo.moveAbsAngle(servo.back_left_hip, 180)
+#     servo.moveAbsAngle(servo.back_left_upper, BL_2_array[i])
+#     servo.moveAbsAngle(servo.back_left_lower, BL_3_array[i])
     
-    time.sleep(0.01)
+#     time.sleep(0.01)
 # servo.moveAbsAngle(servo.front_left_hip, FL_1_array[0])
 # servo.moveAbsAngle(servo.back_left_upper, 100)
 # servo.moveAbsAngle(servo.back_left_lower, 180)
+
+
+## HIP
+for i in range(len(BL_1_array)):
+    print(FL_1_array[i],BL_1_array[i],BR_1_array[i],FR_1_array[i])
+    servo.moveAbsAngle(servo.front_left_hip, FL_1_array[i])
+    servo.moveAbsAngle(servo.back_left_hip, BL_1_array[i])
+    servo.moveAbsAngle(servo.back_right_hip, BR_1_array[i])
+    servo.moveAbsAngle(servo.front_right_hip, FR_1_array[i])
+
+    
+#     time.sleep(0.01)
